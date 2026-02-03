6bf9238297c7948c16aba0a1e6b2145865680ad2
Raiyan-Mirza <raiyanmirzayt@gmail.com>
2026-02-03 15:33:28 +0530

    okk

---
+++ classsprog/day9.js
@@ -0,0 +1,15 @@
+    const task= document.getElementById("profile");
+    task.style.backgroundColor= "tomato";
+    task.style.padding="15px";
+    task.style.textAlign="Center";
+
+const task2 = document.getElementsByClassName("prof");
+console.log(task2);
+ for (let i =0;i<task2.length;i++) {
+    task2[i].style.color="purple"
+    task2[i].style.fontSize="40px";
+ }
+ console.log(task2.length)
+
+
+
